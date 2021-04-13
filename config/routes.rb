118a5a7e18e4322723d3d60f3ba12d962a2b739c
Alexandra Boedtker <alexboedtker@gmail.com>
2021-04-13 18:00:24 +0200

    styling on index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # get 'articles/edit'
-  # get 'articles/index'
-  # get 'articles/new'
-  # get 'articles/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
 end
